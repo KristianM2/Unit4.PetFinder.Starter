bc1f64ab91cf66c9a2cdc4870609db158b22b066
KristianM2 <146405357+KristianM2@users.noreply.github.com>
2024-01-31 17:54:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const pets = require('./data');
 // init express app
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = 8080;
+
+app.use(express.static('public'));
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
@@ -21,31 +24,34 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    res.json(pets);
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
+    const owner = req.query.owner;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.json(pet);
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.json(pet);
 });
 
 app.listen(PORT, () => {
